92b105d1687f8eb5338b63d7e9c8c13fe06327a4
DevAnas19 <anasansari211119@gmail.com>
2026-05-15 20:40:07 +0530

    Deploying the Website

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Change this URL to your Render backend URL once deployed.
 // Example: const API_URL = "https://your-backend.onrender.com";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://nexus-backend-t5jb.onrender.com";
